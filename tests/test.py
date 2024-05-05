45b9b8398b9e94d1b47bdf78a53c935209f409df
Mihail20052005 <108936552+Mihail20052005@users.noreply.github.com>
2024-05-05 15:43:34 +0300

    fix: tests

--- tests/test.py
+++ tests/test.py
@@ -47,24 +47,24 @@ class TestGraphStructure(unittest.TestCase):
 
 
 
-# class TestGUI(unittest.TestCase):
+class TestGUI(unittest.TestCase):
+
+    def setUp(self):
+        self.app = QApplication([])
 #
-#     def setUp(self):
-#         self.app = QApplication([])
-#
-#     def test_main_window(self):
-#         main_window = MainWindow()
-#         main_window.combo_mode.setCurrentIndex(0)
-#         main_window.input_source.setText("1")
-#         main_window.input_target.setText("5")
-#         main_window.input_num_nodes.setText("10")
-#         main_window.input_num_edges.setText("20")
-#         main_window.generate_random_graph()
-#         main_window.find_shortest_path_()
-#         self.assertIsNotNone(main_window.graph)
-#
-#     def tearDown(self):
-#         self.app.quit()
+    def test_main_window(self):
+        main_window = MainWindow()
+        main_window.combo_mode.setCurrentIndex(1)
+        main_window.input_source.setText("1")
+        main_window.input_target.setText("5")
+        main_window.input_num_nodes.setText("10")
+        main_window.input_num_edges.setText("20")
+        main_window.generate_random_graph()
+        main_window.find_shortest_path_()
+        self.assertIsNotNone(main_window.graph)
+
+    def tearDown(self):
+        self.app.quit()
 
 
 if __name__ == '__main__':
